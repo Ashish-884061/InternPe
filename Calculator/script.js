a8cb523ba647213e885d92ea872de5639a126dc6
Ashish-884061 <76278675+Ashish-884061@users.noreply.github.com>
2023-08-23 18:04:13 +0530

    Add files via upload

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,22 +1,51 @@
 const display = document.getElementById("display");
-const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    display.value += button.textContent;
-  });
-});
+function appendToDisplay(value) {
+  display.value += value;
+}
 
-document.addEventListener("keydown", event => {
-  if (event.key.match(/[0-9+\-*/.%]/)) {
-    display.value += event.key;
-  } else if (event.key === "Enter") {
-    try {
-      display.value = eval(display.value);
-    } catch (error) {
-      display.value = "Error";
+function clearDisplay() {
+  display.value = "";
+}
+
+function handleBackspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function handleKeyboardInput(key) {
+  if (key.match(/[0-9+\-*/.%]/) || key === "Enter") {
+    if (key === "Enter") {
+      calculateResult();
+    } else {
+      appendToDisplay(key);
     }
-  } else if (event.key === "Backspace") {
+  } else if (key === "Backspace") {
     display.value = display.value.slice(0, -1);
   }
+}
+
+document.addEventListener("keydown", event => {
+  const key = event.key;
+  handleKeyboardInput(key);
+});
+
+// Add event listeners to buttons
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+    if (buttonValue === "C") {
+      clearDisplay();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    }
+  });
 });
